5a6883086f53b42f9c47a0e62c2be92e562d0a3f
mdyasir1 <mdyasir4145@gmail.com>
2025-08-24 16:33:40 +0530

    fixed build issues-4th time

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,25 @@
 // types/index.ts
+
+// types/next-auth.d.ts
+import { DefaultSession, DefaultUser } from "next-auth";
+import { User as CustomUser } from "@/types"; // Import your custom User type
+
+declare module "next-auth" {
+  /**
+   * The shape of the user object returned in the session.
+   * Extends the default user to include all your custom fields.
+   */
+  interface User extends CustomUser {}
+
+  /**
+   * The shape of the session object.
+   * Adds your full custom user object to the session.
+   */
+  interface Session {
+    user: CustomUser & DefaultSession["user"];
+  }
+}
+
 export type ID = string;
 
 export interface User {
